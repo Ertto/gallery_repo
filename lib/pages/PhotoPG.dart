5872bdd213944494757d5ddaa95fe12eba8e5194
Ertti <Maaax1333@mail.ru>
2020-03-16 20:24:16 +0300

    key
    add key to constructor PhotoPG

--- lib/pages/PhotoPG.dart
+++ lib/pages/PhotoPG.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class PhotoPG extends StatelessWidget {
   final String _photo;
 
-  PhotoPG(this._photo);
+  PhotoPG(this._photo, {Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
